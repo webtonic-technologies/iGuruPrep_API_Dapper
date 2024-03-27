3ab7675db8dbfe3c77da5bfbfb09c6ead048f5c6
VinitaWebtonic <vinita@webtonictechnologies.com>
2024-03-27 15:59:53 +0530

    user registration

---
+++ UserManagement_API/Services/Implementations/UserRegistrationServices.cs
@@ -0,0 +1,27 @@
+﻿using Config_API.DTOs.ServiceResponse;
+using UserManagement_API.DTOs.Registration;
+using UserManagement_API.Repository.Interfaces;
+
+namespace UserManagement_API.Services.Implementations
+{
+    public class UserRegistrationServices : IUserRegistrationServices
+    {
+        private readonly IUserRegistrationRepository _userRegistrationRepository;
+
+        public UserRegistrationServices(IUserRegistrationRepository userRegistrationRepository)
+        {
+            _userRegistrationRepository = userRegistrationRepository;
+        }
+        public async Task<ServiceResponse<string>> UserRegistration(UserRegistrationDto request)
+        {
+            try
+            {
+                return await _userRegistrationRepository.UserRegistration(request);
+            }
+            catch (Exception ex)
+            {
+                return new ServiceResponse<string>(false, ex.Message, string.Empty, 500);
+            }
+        }
+    }
+}
