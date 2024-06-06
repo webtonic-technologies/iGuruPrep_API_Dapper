9ac694ea8d28dd7d0c698a8e8bb5fe99a964873b
abcom <abcom@DESKTOP-TCLFLCH>
2024-06-06 16:37:14 +0530

    pagination

--- Config_API/Repository/Interfaces/IBoardRepository.cs
+++ Config_API/Repository/Interfaces/IBoardRepository.cs
@@ -1,10 +1,11 @@
-﻿using Config_API.DTOs.ServiceResponse;
+﻿using Config_API.DTOs;
+using Config_API.DTOs.ServiceResponse;
 
 namespace Config_API.Repository.Interfaces
 {
     public interface IBoardRepository
     {
-        Task<ServiceResponse<List<Board>>> GetAllBoards();
+        Task<ServiceResponse<List<Board>>> GetAllBoards(GetAllBoardsRequest request);
         Task<ServiceResponse<Board>> GetBoardById(int id);
         Task<ServiceResponse<string>> AddUpdateBoard(Board request);
         Task<ServiceResponse<bool>> StatusActiveInactive(int id);
