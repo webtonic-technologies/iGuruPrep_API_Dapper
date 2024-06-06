eb9dce3503a1f7965ae07a520590e15e2748f50d
abcom <abcom@DESKTOP-TCLFLCH>
2024-06-06 16:27:26 +0530

    changes

---
+++ Config_API/Models/ContentIndexTopics.cs
@@ -0,0 +1,16 @@
+﻿namespace Config_API.Models
+{
+    public class ContentIndexTopics
+    {
+//        public int ContInIdTopic {  get; set; }
+//ContentIndexId int,
+//ContentName_Topic varchar(1000),
+//Status bit,
+//IndexTypeId int,
+//CreatedOn Datetime,
+//CreatedBy varchar(100),
+//ModifiedOn Datetime,
+//ModifiedBy varchar(100),
+//EmployeeId int,
+    }
+}
